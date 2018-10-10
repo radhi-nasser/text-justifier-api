b129d1a58128369411f17d647f96a3a878c4a315
Radhi NASSER <radhinasser@msstn.net>
2018-10-10 08:32:40 +0100

    update commands in package.json

--- public/api_project.js
+++ public/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-10-10T07:21:08.831Z",
+    "time": "2018-10-10T07:32:24.557Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
